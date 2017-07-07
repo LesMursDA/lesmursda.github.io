04ce4c0a27b13b13013ae873e766e2aa824e077d
Naia <naia.alt@gmail.com>
2017-07-07 22:43:07 +0200

    coworking section

---
+++ coworking/script.js
@@ -0,0 +1,216 @@
+function initMap() {
+  var cowork = {
+    lat: 49.0444012,
+    lng: -1.582036
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    // center: cowork,
+    center: new google.maps.LatLng(49.0500000, -0.800000),
+    zoom: 8,
+    scrollwheel: false,
+    navigationControl: true,
+    streetViewControl: true,
+    mapTypeControl: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{
+        elementType: 'geometry',
+        stylers: [{
+          color: '#ebe3cd'
+        }]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#523735'
+        }]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#f5f1e6'
+        }]
+      },
+      {
+        featureType: 'administrative',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#c9b2a6'
+        }]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#dcd2be'
+        }]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#ae9e90'
+        }]
+      },
+      {
+        featureType: 'landscape.natural',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#dfd2ae'
+        }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#dfd2ae'
+        }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#93817c'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry.fill',
+        stylers: [{
+          color: '#a5b076'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#447530'
+        }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#f5f1e6'
+        }]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#fdfcf8'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#f8c967'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#e9bc62'
+        }]
+      },
+      {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#e98d58'
+        }]
+      },
+      {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#db8555'
+        }]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#806b63'
+        }]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#dfd2ae'
+        }]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#8f7d77'
+        }]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#ebe3cd'
+        }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#dfd2ae'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry.fill',
+        stylers: [{
+          color: '#b9d3c2'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#92998d'
+        }]
+      }
+    ]
+  });
+
+  var contentString = '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<div id="bodyContent">' +
+    '<p><b>Address</b><br>' +
+    '21 Rue du Feugré<br>' +
+    '50230 Agon-Coutainville</p>' +
+    '<p>Situé à Agon-Coutainville, station balnéaire renommée de la côte ouest du Cotentin en Normandie, cet espace de coworking unique permet de combiner travail et détente.' +
+    '<br>A seulement 3h30 de Paris, ce lieu est aussi idéal pour des équipes projet ayant à travailler ensemble ponctuellement et souhaitant sortir d’un quotidien urbain pour s’immerger dans un environnement propice à la créativité. Les Murs d’Agon proposent aussi deux lofts tout équipés de 2 à 4 personnes permettant un hébergement sur place.</p>'
+    '</div>' +
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 300
+  });
+
+  var marker = new google.maps.Marker({
+    position: cowork,
+    map: map,
+    title: "Les Murs D'Agon"
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+
+function bindInfoWindow(marker, map, infoWindow, strDescription) {
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(strDescription);
+    infoWindow.open(map, marker);
+  });
+}
